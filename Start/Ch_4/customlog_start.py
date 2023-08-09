a8f24887aa5e1dd0a80495bd48688065460e37a2
mrsvllmr <96266054+mrsvllmr@users.noreply.github.com>
2023-08-09 17:43:52 +0200

    Finished chapter 4 and the course

--- Start/Ch_4/customlog_start.py
+++ Start/Ch_4/customlog_start.py
@@ -2,14 +2,23 @@
 
 import logging
 
+
 # TODO: add another function to log from
+def another_function():
+    logging.debug("This is a debug-level log message", extra=extdata)
 
 
 # set the output file and debug level, and
 # TODO: use a custom formatting specification
+fmtstr = "User: %(user)s %(asctime)s: %(levelname)s: %(funcName)s Line:%(lineno)d %(message)s"
+datestr = "%m/%d/%Y %I:%M:%S %p"
+extdata = {"user": "mrsvllmr@fake.com"}
 logging.basicConfig(filename="output.log",
-                    level=logging.DEBUG)
+                    level=logging.DEBUG,
+                    filemode="w",
+                    format=fmtstr,
+                    datefmt=datestr)
 
-logging.info("This is an info-level log message")
-logging.warning("This is a warning-level message")
-
+logging.info("This is an info-level log message", extra=extdata)
+logging.warning("This is a warning-level message", extra=extdata)
+another_function()
